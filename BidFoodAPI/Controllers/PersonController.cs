0c09f7135b173ccffa3c0f8345cdc3dda32115e5
DFatima <d.fathima@gmail.com>
2023-11-06 22:47:20 +1300

    Add project files.

---
+++ BidFoodAPI/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BidFoodAPI.Models;
+using Newtonsoft.Json;
+
+namespace BidFoodAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SubmitForm([FromBody] Person personData)
+        {
+            try
+            {
+                // Serialize the submitted data to JSON
+                var jsonData = JsonConvert.SerializeObject(personData);
+
+                // Save JSON data to a flat file
+                string fileName = "personData.json";
+                System.IO.File.AppendAllText(fileName, jsonData);
+
+                return Ok("Form data saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error saving data: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ BidFoodAPI/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BidFoodAPI.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
